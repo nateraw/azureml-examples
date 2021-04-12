7373cd227e99682307a96c84f36707cfd77bf49f
nateraw <nxr9266@g.rit.edu>
2021-04-11 18:58:51 -0600

    :construction: wip

--- tests/test_lightning_mnist_example.py
+++ tests/test_lightning_mnist_example.py
@@ -1,15 +1,15 @@
-# import json
-# import pytest
-# from pathlib import Path
+import json
+import pytest
+from pathlib import Path
 
-# from lightning_mnist_example import score, train
+from lightning_mnist_example import score, train
 
 
-# @pytest.mark.usefixtures('rm_logdir')
-# def test_local_run():
-#     args = train.parse_args('--max_epochs 1 --default_root_dir logs/'.split())
-#     train.main(args)
-#     assert 1 == 1
+@pytest.mark.usefixtures('rm_logdir')
+def test_local_run():
+    args = train.parse_args('--max_epochs 1 --default_root_dir logs/ --batch_size 8'.split())
+    train.main(args)
+    assert 1 == 1
 
 #     ckpt_paths = list(Path('logs/lightning_logs/').glob('**/*.ckpt'))
 #     assert bool(ckpt_paths) == True
