ef14b3eb9674ffb99334bc06c82c9bbdc9159e94
Jirka <jirka.borovec@seznam.cz>
2021-10-12 23:47:27 +0200

    use Fire CLI

--- deploy_on_azureml.py
+++ deploy_on_azureml.py
@@ -1,6 +1,6 @@
-from argparse import ArgumentParser
 from pathlib import Path
 
+import fire
 from azureml.core import Experiment, Run
 from azureml.core.environment import Environment
 from azureml.core.model import InferenceConfig, Model
@@ -8,39 +8,35 @@ from azureml.core.webservice import AciWebservice
 from azureml.core.workspace import Workspace
 
 
-def parse_args(args=None):
-    parser = ArgumentParser()
-    parser.add_argument("--workspace_config", type=str, default="config.json")
-    parser.add_argument("--requirements_file", type=str, default="requirements.txt")
-    parser.add_argument("--experiment_name", type=str, default="test-exp")
-    parser.add_argument("--run_id", type=str, default=None)
-    parser.add_argument("--model_artifact_path", type=str, default="logs/saved_model")
-    parser.add_argument("--score_file", type=str, default="keras_example/score.py")
-    parser.add_argument("--model_name", type=str, default="test-keras-model")
-    parser.add_argument("--service_name", type=str, default="test-keras-service")
-    return parser.parse_args(args)
-
-
-def main(args):
+def main(
+        workspace_config: str = "config.json",
+        requirements_file: str = "requirements.txt",
+        experiment_name: str = "test-exp",
+        run_id: str = None,
+        model_artifact_path: str = "logs/saved_model",
+        score_file: str = "keras_example/score.py",
+        model_name: str = "test-keras-model",
+        service_name: str = "test-keras-service",
+):
 
     # Init workspace object from azureml workspace resource you've created
-    ws = Workspace.from_config(args.workspace_config)
+    ws = Workspace.from_config(workspace_config)
 
     # Point to an experiment
-    experiment = Experiment(ws, name=args.experiment_name)
+    experiment = Experiment(ws, name=experiment_name)
 
-    run = Run(experiment, args.run_id)
+    run = Run(experiment, run_id)
 
     # Register your best run's model
-    model = run.register_model(model_name=args.model_name, model_path=args.model_artifact_path)
+    model = run.register_model(model_name=model_name, model_path=model_artifact_path)
 
     # Create an environment based on requirements
-    env = Environment.from_pip_requirements("my-pip-env", args.requirements_file)
+    env = Environment.from_pip_requirements("my-pip-env", requirements_file)
 
     # Get inference config to configure API's behavior
     inference_config = InferenceConfig(
-        source_directory=Path(args.score_file).parent,
-        entry_script=Path(args.score_file).name,
+        source_directory=Path(score_file).parent,
+        entry_script=Path(score_file).name,
         environment=env,
         enable_gpu=False,
     )
@@ -51,7 +47,7 @@ def main(args):
 
     service = Model.deploy(
         workspace=ws,
-        name=args.service_name,
+        name=service_name,
         models=[model],
         inference_config=inference_config,
         deployment_config=deployment_config,
@@ -62,4 +58,4 @@ def main(args):
 
 
 if __name__ == "__main__":
-    service = main(parse_args())
+    fire.Fire(main)
--- run_on_azureml.py
+++ run_on_azureml.py
@@ -1,6 +1,6 @@
-from argparse import ArgumentParser
 from pathlib import Path
 
+import fire
 from azureml.core import (
     Environment,
     Experiment,
@@ -35,36 +35,31 @@ def find_or_create_compute_target(
     return target
 
 
-def parse_args(args=None):
-    parser = ArgumentParser()
-    parser.add_argument("--workspace_config", type=str, default="config.json")
-    parser.add_argument("--experiment_name", type=str, default="azureml-demo-exp")
-    parser.add_argument("--compute_target_name", type=str, default="my-compute")
-    parser.add_argument("--requirements_file", type=str, default="requirements.txt")
-    parser.add_argument("--script_path", type=str, default="simple_example/run.py")
-    parser.add_argument("--delete_compute", action="store_true", default=False)
-    args, extras = parser.parse_known_args(args)
-    args.script_args = extras
-    return args
-
-
-def main(args):
-    ws = Workspace.from_config(args.workspace_config)
-    exp = Experiment(ws, args.experiment_name)
-    target = find_or_create_compute_target(ws, args.compute_target_name)
-    env = Environment.from_pip_requirements("my-pip-env", args.requirements_file)
+def main(
+        workspace_config: str = "config.json",
+        experiment_name: str = "azureml-demo-exp",
+        compute_target_name: str = "my-compute",
+        requirements_file: str = "requirements.txt",
+        script_path: str = "simple_example/run.py",
+        delete_compute: bool = False,
+        **kwargs,
+):
+    ws = Workspace.from_config(workspace_config)
+    exp = Experiment(ws, experiment_name)
+    target = find_or_create_compute_target(ws, compute_target_name)
+    env = Environment.from_pip_requirements("my-pip-env", requirements_file)
     run_config = ScriptRunConfig(
-        source_directory=Path(args.script_path).parent,
-        script=Path(args.script_path).name,
-        arguments=args.script_args,
+        source_directory=Path(script_path).parent,
+        script=Path(script_path).name,
+        arguments=kwargs,
         compute_target=target,
         environment=env,
     )
     run = exp.submit(run_config)
     run.wait_for_completion(show_output=True)
-    if args.delete_compute:
+    if delete_compute:
         target.delete()
 
 
 if __name__ == "__main__":
-    main(parse_args())
+    fire.Fire(main)
